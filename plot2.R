7a2cea68bb87fe14b2ea493c019a0839fc42f3ae
Cory B <cory@vt.edu>
2014-06-10 15:34:42 -0400

    Plot 2

--- plot2.R
+++ plot2.R
@@ -1,3 +1,6 @@
+
+library(zoo)
+
 #Importing the file
 HPC1 <- read.csv("C:/Grad Classes/Research-ENGE/R/exdata-data-household_power_consumption/household_power_consumption.txt", sep=";",header=TRUE,na.strings="?",stringsAsFactors=FALSE)
 
@@ -11,14 +14,14 @@ HPC2days$Global_active_power<-as.numeric(as.character(HPC2days$Global_active_pow
 HPC2days$DT<-paste(as.character(HPC2days$Date),as.character(HPC2days$Time))
 
 #create object
-HPC2days$DT<-strptime(HPC2days$$DT,"%d/%m/%Y %H:%M:%S")
+HPC2days$DT<-strptime(HPC2days$DT,"%d/%m/%Y %H:%M:%S")
 
 #time series
 TS<-zoo(HPC2days$Global_active_power,HPC2days$DT)
 
 #Create graph
 png(filename="plot2.png",height=480,width=480)
-plot(TS,type="1",ylab="Global Active Power (kilowats)",xlab="")
+plot(TS,ylab="Global Active Power (kilowats)",xlab="")
 dev.off()
 
 
